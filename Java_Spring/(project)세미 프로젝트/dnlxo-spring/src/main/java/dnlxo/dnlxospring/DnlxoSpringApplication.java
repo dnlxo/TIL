b9df6db9e0135bf10f6e7711f2c243e982d8d3f3
dnlxo <wty8798@naver.com>
2021-07-05 06:24:37 +0900

    boot

---
+++ Java_Spring/(project)세미 프로젝트/dnlxo-spring/src/main/java/dnlxo/dnlxospring/DnlxoSpringApplication.java
@@ -0,0 +1,13 @@
+package dnlxo.dnlxospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnlxoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnlxoSpringApplication.class, args);
+	}
+
+}
---
+++ Java_Spring/(project)세미 프로젝트/dnlxo-spring/src/test/java/dnlxo/dnlxospring/DnlxoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dnlxo.dnlxospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnlxoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
